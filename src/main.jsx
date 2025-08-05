7116477edacaa10c4d8efa4b5321bb9eeea6707f
rubeniab <rub_isai@hotmail.com>
2025-08-04 19:56:07 -0600

    Corrección de importación de Styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './styles.css'
+import './Styles.css'
 import ConsultaPredial from './ConsultaPredial'
 
 createRoot(document.getElementById('root')).render(
