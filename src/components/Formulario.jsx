100c6546f38a1db7d766dc934ed7010e35673c0d
rubeniab <rub_isai@hotmail.com>
2025-08-14 18:28:59 -0600

    Prueba de fotografias

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -81,7 +81,7 @@ function Formulario() {
   <div className="modal-overlay">
     <div className="modal-content">
       <button className="close-button" onClick={() => setIsAyudaOpen(false)}>X</button>
-      <img src="/images/RECIBO2.jpg" alt="Ayuda" className="modal-image" />
+      <img src="/images/RECIBO2.png" alt="Ayuda" className="modal-image" />
     </div>
   </div>
 )}
